0db532410018bf71903c069867bc2424c75cf048
Unais <unaissk1234@gmail.com>
2024-09-17 18:34:21 +0530

    project showcase added

---
+++ Projects Showcase/src/project.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    id: 1,
+    name: "Book Shopee",
+    image: "../public/Bookshopee.png",
+    link: "https://bookshopee-react.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Password Generator",
+    image: "../public/Password Generator.png",
+    link: "https://password-generation-react.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Tours Website",
+    image: "../public/Tours.png",
+    link: "https://tours-website-react.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Review Slider",
+    image: "../public/Reviewslider.png",
+    link: "https://reviews-slide-reactjs.netlify.app/",
+  },
+  // Add more projects here
+];
+export default projects;
