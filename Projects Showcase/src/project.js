adbd2a11147116b9b6dfd7063490cb714d2c1c7d
Unais <unaissk1234@gmail.com>
2024-09-18 16:08:14 +0530

    Accordion Added

--- Projects Showcase/src/project.js
+++ Projects Showcase/src/project.js
@@ -2,25 +2,25 @@ const projects = [
   {
     id: 1,
     name: "Book Shopee",
-    image: "../public/Bookshopee.png",
+    image: "./Bookshopee.png",
     link: "https://bookshopee-react.netlify.app/",
   },
   {
     id: 2,
     name: "Password Generator",
-    image: "../public/Password Generator.png",
+    image: "./Password Generator.png",
     link: "https://password-generation-react.netlify.app/",
   },
   {
     id: 3,
     name: "Tours Website",
-    image: "../public/Tours.png",
+    image: "./Tours.png",
     link: "https://tours-website-react.netlify.app/",
   },
   {
     id: 4,
     name: "Review Slider",
-    image: "../public/Reviewslider.png",
+    image: "./Reviewslider.png",
     link: "https://reviews-slide-reactjs.netlify.app/",
   },
   // Add more projects here
